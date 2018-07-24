1446d2443f9d51bf009bf9e0fa28d66a2f124687
ycxmichael <ycxmichael@gmail.com>
2018-07-23 22:19:30 -0700

    insert scripts for table genre, have, isparentof, label, under

---
+++ insert_havegenre_script.sql
@@ -0,0 +1,9 @@
+INSERT INTO Have (AlbumID,SongName,GenreName)
+VALUES
+(1,'Jouse Flow','Deep House'),
+(1,'Private Selec.','Deep House'),
+(2,'Ching','Rap'),
+(2,'Trap Nation','Trap'),
+(3,'Fufusu','Techno'),
+(4,'Boss','House'),
+(5,'Storm','Pop')
