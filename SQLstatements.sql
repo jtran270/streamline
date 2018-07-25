1f553baac1e00c8bf22bd56334ed78aa129d5787
Juno Sohn <juno.sohn@gmail.com>
2018-07-24 19:29:19 -0700

    SQL queries - updated

--- SQLstatements.sql
+++ SQLstatements.sql
@@ -11,89 +11,119 @@ SELECT PlaylistName
 FROM CreatePlaylist
 WHERE CreatePlaylist.UserID = getUserID() -- UserID passed from view
 
--- Output SongName, Artist StageName, and TrackLength in the user playlist
--- returnPlaylistName = whatever PlaylistName is returned from above query
 
-SELECT CS.SongName, AID.StageName, CS.TrackLength
-FROM ContainSong CS, ArtistUserID AID
-JOIN CreateAlbum C ON 
-	CS.AlbumID = C.AlbumID AND 
-	C.UserID = AID.UserID AND
+-- Output playlist name, and all song-related info for user logged in
+SELECT PIS.playlistname, PIS.songname, AID.stagename, A.albumname, CS.tracklength
+FROM playlistincludessongs PIS
+JOIN containsong CS ON CS.albumid = PIS.albumid AND CS.songname = PIS.songname
+JOIN Album A ON A.albumid = CS.albumid 
+JOIN createalbum CA ON CA.albumid = A.albumid
+JOIN ArtistUserId AID ON AID.userid = CA.userid
+WHERE PIS.userid = 6 -- (replace with ID of user logged in) or...
+-- WHERE PIS.playlistname = 'Chill' --(replace with playlist name)
 
-	-- test this part
-	returnPlaylistName IN (
-		SELECT PlaylistName 
-		FROM Include
-		WHERE UserID = AID.UserID)
+
+
+-- **** select all playlists from that user
 
 
 
 -- Find all songs under chosen genre
--- genre = 'Techno';
-SELECT SongName
-FROM ContainSong, Genre
-WHERE ContainSong.SongName = Genre.SongName AND
-	Genre.GenreName = getGenreName()
-
+-- genre = 'Deep House';
+SELECT CS.SongName, AID.stagename, A.albumname
+FROM ContainSong CS
+JOIN havesongs HS ON HS.albumid = CS.albumid AND HS.songname = CS.songname
+JOIN Album A ON A.albumid = CS.albumid 
+JOIN createalbum CA ON CA.albumid = A.albumid
+JOIN ArtistUserId AID ON AID.userid = CA.userid
+WHERE HS.genrename = 'Deep House' --(replace with genre selection)
 
 
 
 -- AGGREGATION:
 -- Find total number of songs in the user playlist
 SELECT COUNT(*)
-FROM Include 
-JOIN CreatePlaylist ON 
-	Include.UserID = CreatePlaylist.UserID AND 
-	Include.PlaylistName = CreatePlaylist.PlaylistName
-JOIN ListenerUserID ON CreatePlaylist.UserID = ListenerUserID.UserID
-WHERE Include.UserID = getUserID()
-
+FROM playlistincludessongs PIS
+JOIN CreatePlaylist CP ON 
+	PIS.UserID = CP.UserID AND 
+	PIS.PlaylistName = CP.PlaylistName
+JOIN ListenerUserID LID ON CP.UserID = LID.UserID
+WHERE PIS.UserID = 6
+-- AND PIS.playlistname = 'Chill'
 
 
 -- NESTED AGGREGATE WITH GROUP-BY:
--- Return names of all songs in a specified genre that has more than 5 songs within that genre
-SELECT CS.SongName
-FROM ContainSong CS
-WHERE CS.SongName IN (
-	SELECT H.SongName
-	FROM Have H
-	WHERE H.GenreName = getGenreName()
-	GROUP BY H.GenreName
-	HAVING COUNT(*) > 5)
-
+-- Return the artist info and the name of their album that has more than 2 songs that are 3 minutes or longer
+SELECT AN.stagename, AN.firstname, AN.lastname, A.albumname
+FROM artistname AN
+JOIN artistuserid AID ON AID.stagename = AN.stagename
+JOIN createalbum CA ON CA.userid = AID.userid
+JOIN album A ON A.albumid = CA.albumid
+WHERE A.albumid IN (
+		SELECT CS.albumid
+		FROM containsong CS
+		WHERE CS.tracklength >= '00:03:00'
+		GROUP BY CS.albumid
+		HAVING COUNT(*) > 2
+	)
 
 
 -- UPDATE:
 -- Change all Artist StageName to StageNameEdit (input)
 UPDATE ArtistUserID
-SET StageName = StageNameEdit
+SET StageName = 'JCUBE2' --input2
+WHERE StageName = 'JCUBE' --input1
 
 UPDATE ArtistName
-SET StageName = StageNameEdit
+SET StageName = 'JCUBE2' --input2
+WHERE StageName = 'JCUBE' --input1
 
 
 -- DELETE:
--- Delete song from user's playlists 
--- (still remains in DB though)
-DELETE FROM Include
-WHERE UserID = getUserID() AND
-	SongName = inputSongName()
+-- Delete song from user's playlists (still remains in DB though)
+DELETE FROM playlistincludessongs
+WHERE UserID = 9 --get userID from view 
+	AND SongName = 'Jouse Flow' --input songname or select songname from list
+
+-- Delete user's chosen playlist
+-- parent table createplaylist deletes child table playlistincludessongs's entries
+DELETE FROM createplaylist 
+WHERE userid = 9 --get userID from view
+	AND playlistname = 'Favorites' --input playlistname or select playlistname from list
+
+/*
+INSERT INTO createplaylist VALUES (9, 'Favorites')
+INSERT INTO playlistincludessongs VALUES (2, 'Trap Nation', 9, 'Favorites')
+*/
+
 
 
 
 -- DIVISION:
--- Return songs that are in ALL of playlists 
+-- Return songs and their info that are in ALL of playlists 
+-- select song names such that there is no user that does not have the song in their playlist"
+SELECT DISTINCT CS.SongName, AID.stagename, A.albumname, CS.tracklength
+FROM playlistincludessongs PIS
+JOIN containsong CS ON CS.albumid = PIS.albumid AND CS.songname = PIS.songname
+JOIN Album A ON A.albumid = CS.albumid 
+JOIN createalbum CA ON CA.albumid = A.albumid
+JOIN ArtistUserId AID ON AID.userid = CA.userid
 
--- // select song names such that there is no user that does not have the song in their playlist 
-SELECT CS.SongName
-FROM ContainSong CS
 WHERE NOT EXISTS (
 	(SELECT CP.UserID FROM CreatePlaylist CP)
---or(SELECT CP.PlaylistName FROM CreatePlaylist CP)
 	EXCEPT
-	(SELECT I.SongName 
-	FROM Include I 
-	WHERE I.SongName = CS.SongName AND 
-		I.AlbumID = CS.AlbumID)
+	(SELECT PIS.userid 
+	FROM playlistincludessongs PIS
+	WHERE PIS.SongName = CS.SongName AND 
+		PIS.AlbumID = CS.AlbumID)
 )
 
+
+/* insert all of these first before querying
+INSERT INTO playlistincludessongs VALUES (1, 'Bliss', 6, 'Chill');
+INSERT INTO playlistincludessongs VALUES (1, 'Bliss', 8, 'Sad song for a sad boi');
+INSERT INTO playlistincludessongs VALUES (1, 'Bliss', 9, 'Study playlist');
+INSERT INTO playlistincludessongs VALUES (1, 'Bliss', 9, 'Moms home playlist');
+INSERT INTO playlistincludessongs VALUES (1, 'Bliss', 10, 'Gospels only Praise the Holy Father');
+INSERT INTO playlistincludessongs VALUES (1, 'Bliss', 9, 'Favorites');
+*/
